87865c1790ece0baae33718e1d8569ba0cdca839
Matheus <mathfernando@hotmail.com>
2022-05-10 11:28:15 -0300

    teste fonte produção - 1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84bbda72d3e00c18"; }
+/******/ 		__webpack_require__.h = function() { return "15eb3da74ad4352d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/fonts.tsx
+++ src/pages/fonts.tsx
@@ -10,7 +10,7 @@ export const Fonts = () => (
       font-style: normal;
       font-weight: 400;
       font-display: swap;
-      src: url('./fonts/AndesBook.woff2') format('woff2');
+      src: url('./static/AndesBook.woff2') format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
     @font-face {
@@ -18,7 +18,7 @@ export const Fonts = () => (
       font-style: normal;
       font-weight: 600;
       font-display: swap;
-      src: url('./fonts/AndesMedium.woff2') format('woff2');
+      src: url('./static/AndesMedium.woff2') format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
     @font-face {
@@ -26,7 +26,7 @@ export const Fonts = () => (
       font-style: normal;
       font-weight: 700;
       font-display: swap;
-      src: url('./fonts/AndesBold.woff2') format('woff2');
+      src: url('./static/AndesBold.woff2') format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
     `}
