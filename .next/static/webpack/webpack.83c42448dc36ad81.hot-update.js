ceedb6b00b1ac4f8e898a5486093732bce89e8f3
Matheus <mathfernando@hotmail.com>
2022-06-15 16:10:24 -0300

    Página 404 e icon do logo

--- .next/static/webpack/webpack.83c42448dc36ad81.hot-update.js
+++ .next/static/webpack/webpack.83c42448dc36ad81.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e6c64c283eeab3f8"; }
+/******/ 	__webpack_require__.h = function() { return "170df98e2249e3c2"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
