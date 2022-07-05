27975b69c774e26c98b64414a42f0f3612295289
Matheus <mathfernando@hotmail.com>
2022-07-05 14:28:08 -0300

    teste gif 3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "264413b4e0fd93b9"; }
+/******/ 		__webpack_require__.h = function() { return "1845e328a47482c5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
