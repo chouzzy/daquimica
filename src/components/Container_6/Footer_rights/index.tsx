1d6459007f3d93f781a80bedf18e1e5bb981fed6
Matheus <mathfernando@hotmail.com>
2022-08-22 14:26:50 -0300

    Delete src directory

--- src/components/Container_6/Footer_rights/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, useBreakpointValue } from "@chakra-ui/react";
-import React from "react";
-
-export function FooterRights() { 
-
-   return (
-         <Text textAlign='left' w='90vw' py={3} fontSize='0.8rem'>Todos os direitos reservados © Minuzzi</Text>
-   )
-}
